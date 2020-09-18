6ca9a2246540831f0d2f3c0a38af330381dcecd0
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-09-18 13:22:52 +0200

    riordinate cartelle, aggiunta classe per le costanti

--- src/SteganografiaTesto/MainClass.java
+++ src/SteganografiaTesto/MainClass.java
@@ -1,16 +1,18 @@
 package SteganografiaTesto;
 
+import utils.Constants;
+
 public class MainClass {
 	public static void main(String[] args) {
 		// Cripto il messaggio nell'immagine, e poi la salvo
 		String testo = "Sono un gattino";
-		Image image = new Image("src/gatto.png");
+		Image image = new Image(Constants.PATH_TO_INPUT+"gatto.png");
 
 		image.encryptMessage(testo);
-		image.saveImage("src/gattoCriptato.png");
+		image.saveImage(Constants.PATH_TO_OUTPUT+"gattoCriptato.png");
 
 		// Decripto l'immagine e stampo il risultato.
-		Image dec = new Image("src/gattoCriptato.png");
+		Image dec = new Image(Constants.PATH_TO_OUTPUT+"gattoCriptato.png");
 		System.out.println(dec.decryptMessage());
 	}
 }
---
+++ src/utils/Constants.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Constants {
+
+	public final static String PATH_TO_INPUT = "input/";
+	public final static String PATH_TO_OUTPUT = "output/";
+	
+}
