b09a7b3e0b5913f38871f9eb8b42ddf9c9e7ace4
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-09-18 16:40:48 +0200

    aggiunto percorso per il secret txt

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 
 	public final static String PATH_TO_INPUT = "input/";
 	public final static String PATH_TO_OUTPUT = "output/";
-	
+
+	public final static String SECRET_TEXT = Constants.PATH_TO_INPUT+ "secret.txt";
 }
