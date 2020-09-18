eb0e4720b1fbe1a5cf699c760a3340b601689d17
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-09-18 11:24:33 +0200

    fixed bug

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,7 +2,7 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		int x = 0;
 		int y = 0;
 		ImageManipulator img = new ImageManipulator("src/input.png");
